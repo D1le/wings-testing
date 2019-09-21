6da33026eaebdbabb2411eb08de1d2f7506417b8
Alexey Lapin <alexey-lapin@protonmail.com>
2019-09-21 23:18:16 +0300

    travis docs

--- src/docs/.vuepress/config.js
+++ src/docs/.vuepress/config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  dest: "build/site"
+  description: "wings framework documentation",
+  dest: "build/site",
+  title: "wings framework",
+  themeConfig: {
+    docsDir: "src/docs",
+    repo: "alexey-lapin/wings-testing",
+    sidebar: [
+      // "/",
+      "/introduction/"
+    ]
+  }
 }
